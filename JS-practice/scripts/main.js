db4cf006ac74d47a186dda6a1deaa051e490fdf6
balajiudy-github <balu.udy@gmail.com>
2022-09-13 19:24:54 +0530

    ticket#1309

---
+++ JS-practice/scripts/main.js
@@ -0,0 +1,21 @@
+var a = 10, b = 20;
+var s = "Hello World";
+var l = s.length;
+// arithmatic
+document.getElementById('p-1').innerHTML = 'adding ' + (a + b);
+// string length
+document.getElementById('head1-1').innerHTML = 'length of the String s = ' + l;
+// Math function
+document.getElementById('head1-2').innerHTML = Math.random();
+// string interpolation
+var s = 'BALAJI';
+var greet = `hello ${s} how are you`;
+document.getElementById('head1-3').innerHTML = greet;
+
+let price = 10.5;
+let day = prompt("enter the day");
+
+day === "monday" ? price += 10 : price -= 5;
+
+document.getElementById('head1-4').innerHTML = `${day} ${price}rs`;
+
