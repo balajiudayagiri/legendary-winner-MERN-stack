64e69ddbed9c95fe2de718995e0fc4b1e91a748d
balajiudy-github <balu.udy@gmail.com>
2022-09-16 19:00:32 +0530

    ticket#1609

--- JS-practice/scripts/app.js
+++ JS-practice/scripts/app.js
@@ -1,22 +1,22 @@
-function message() {
-    return 'hello';
-}
+// function message() {
+//     return 'hello';
+// }
 
-document.getElementById('head1-4').innerHTML = message();
+// document.getElementById('head1-4').innerHTML = message();
 
-const person = {
-    firstName: "Udayagiri",
-    lastName: "Balaji",
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
-    },
-    age: 24,
-    place: "banglore"
-};
-// let hhh = "hello" + fullName + " " + age + " " + place + " ";
+// const person = {
+//     firstName: "Udayagiri",
+//     lastName: "Balaji",
+//     fullName: function () {
+//         return this.firstName + " " + this.lastName;
+//     },
+//     age: 24,
+//     place: "banglore"
+// };
+// // let hhh = "hello" + fullName + " " + age + " " + place + " ";
 
-function details(a) {
-    console.log("hello");
-}
-details(fullName);
+// function details(name) {
+//     console.log(name);
+// }
+// details("fullName");
 
