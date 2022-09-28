73a5f2959e8cfe7c9a4f4caef68259e1a43c4455
balajiudy-github <balu.udy@gmail.com>
2022-09-28 18:51:51 +0530

    update_28-sep_06:51_pm

---
+++ classAssignment/27-sep/script/main.js
@@ -0,0 +1,12 @@
+
+function sayHelloR() {
+    const fname = document.getElementById('fname').value;
+    const email = document.getElementById('e-mail1').value;
+
+    alert(`hello ${fname},\n your account is registerd with the Mailing ID :- ${email}`);
+}
+
+function sayHelloL() {
+    const fname1 = document.getElementById('e-mail2').value;
+    alert(`hello ${fname1},\n you are loged in`);
+}
