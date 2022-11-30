d7571f12e017748e2df3adf4939bcb6d9ec37051
balajiudy-github <balu.udy@gmail.com>
2022-11-30 19:40:07 +0530

    update_30-nov_07:39_pm

--- Reactjs/react-project1/src/components/Main_Web_Folder/login_regestration_Component/LoginPopUp.js
+++ Reactjs/react-project1/src/components/Main_Web_Folder/login_regestration_Component/LoginPopUp.js
@@ -30,24 +30,24 @@ export default class LoginPopUp extends Component {
         if (!data.email) {
             this.setState({
                 mailBoxError: 'Please enter Email',
-                mailBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99', color: 'white' }
+                mailBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99' }
             })
         } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(data.email)) {
             this.setState({
                 mailBoxError: 'Please enter a valid Email format',
-                mailBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99', color: 'white' }
+                mailBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99' }
             })
 
         }
         if (!data.password) {
             this.setState({
                 passwordBoxError: 'Please enter password',
-                passwordBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99', color: 'white' }
+                passwordBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99' }
             })
         } else if (!(data.password.length > 7 && data.password.length < 16)) {
             this.setState({
                 passwordBoxError: 'Must have 8 charectors and atleast have 1 lowerCase, upperCase, number,@,#,$',
-                passwordBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99', color: 'white' }
+                passwordBoxErrorStyle: { border: '1px solid red', backgroundColor: '#e7aeae99' }
             })
         }
     }
