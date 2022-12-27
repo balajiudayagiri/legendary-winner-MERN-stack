fd3db67741ca078fda810f85c47c41f559a4c949
Balaji Udayagiri <69666019+balajiudayagiri@users.noreply.github.com>
2022-12-27 22:18:12 +0530

    update

--- Reactjs/react-redux/src/App.js
+++ Reactjs/react-redux/src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
-import { Provider } from "react-redux";
-import FakeStore from "./componentForRedux/fakeStore/FakeStore";
-import { store } from "./componentForRedux/store/store";
+// import "./App.css";
+import Cart from "./PracticeComponent/Cart/Cart";
+import Layout from "./PracticeComponent/Layout/Layout";
+import Products from "./PracticeComponent/Shop/Products";
+// fakeStore-redux
+// import { Provider } from "react-redux";
+// import FakeStore from "./componentForRedux/fakeStore/FakeStore";
+// import { store } from "./componentForRedux/store/store";
+// ----
 function App() {
+  // return (
+  //   <Provider store={store}>
+  //     <FakeStore />
+  //   </Provider>
+  // );
   return (
-    <Provider store={store}>
-      <FakeStore />
-    </Provider>
+    <>
+      <Layout>
+        <Cart />
+        <Products />
+      </Layout>
+    </>
   );
 }
 export default App;
